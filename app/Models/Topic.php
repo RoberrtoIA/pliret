954427a143213655cb52f370271cb1050700c05d
Roberto Moreno <robertoantoniomoreno1999@gmail.com>
2023-10-10 21:29:35 -0600

    Topic model

---
+++ app/Models/Topic.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Topic extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = ['index', 'title', 'description', 'content', 'module_id'];
+
+    public function module()
+    {
+        return $this->belongsTo(Module::class);
+    }
+
+    public function questions()
+    {
+        return $this->hasMany(Question::class);
+    }
+}
