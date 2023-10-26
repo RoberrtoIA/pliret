c20e27a230eb934fca15ebb69d1610511231eea2
Roberto Moreno <robertoantoniomoreno1999@gmail.com>
2023-10-26 13:56:45 -0600

    Question Update

--- app/Http/Controllers/V1/User/QuestionController.php
+++ app/Http/Controllers/V1/User/QuestionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\V1\User;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\StoreQuestionRequest;
+use App\Http\Requests\UpdateQuestionRequest;
 use App\Http\Resources\QuestionResource;
 use App\Models\Question;
 use Illuminate\Http\Request;
@@ -24,27 +25,18 @@ class QuestionController extends Controller
         return (new QuestionResource($question->load('topic')))->response()->setStatusCode(201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(Question $question)
     {
-        //
+        return new QuestionResource($question->load('topic'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(Question $question, UpdateQuestionRequest $request)
     {
-        //
+        $attributes = $request->validated();
+
+        $question->update($attributes);
+
+        return new QuestionResource($question->load('topic'));
     }
 
     /**
---
+++ app/Http/Requests/UpdateQuestionRequest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Http\Requests;
+
+class UpdateQuestionRequest extends StoreQuestionRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function rules()
+    {
+        return replaceRequiredByFilledRules(parent::baseRules());
+    }
+}
