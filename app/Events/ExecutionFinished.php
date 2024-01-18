e1eb82eb64d085f0937fcbeab24cd1eddbc361b8
Roberto Moreno <robertoantoniomoreno1999@gmail.com>
2024-01-17 23:05:03 -0600

    Redundancia en evento

--- app/Events/ExecutionFinished.php
+++ app/Events/ExecutionFinished.php
@@ -20,25 +20,3 @@ class ExecutionFinished
     {
     }
 }
-<?php
-
-namespace App\Events;
-
-use App\Models\Execution;
-use Illuminate\Broadcasting\InteractsWithSockets;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
-
-class ExecutionFinished
-{
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    /**
-     * Create a new event instance.
-     *
-     * @return void
-     */
-    public function __construct(public readonly Execution $execution)
-    {
-    }
-}
